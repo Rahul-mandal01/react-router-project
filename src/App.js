02f895abe5518952b19d7123951f676ddb924aec
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-30 20:06:06 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      Hello Jii
+    <div>
+        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+
+
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path='/signup' element={<Signup  setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+        </Routes>
+
     </div>
   );
 }
