28d5ab7f3b26bd66bdb15ebc21ae616d140b7281
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-31 23:43:21 +0530

    uploading tailwind CSS codes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = (props) => {
     let setIsLoggedIn = props.setIsLoggedIn;
 
   return (
-    <div className='flex justify-evenly' >
+    <div className='flex justify-between items-center w-11/12 max-w-[1160px] py-4 mx-auto ' >
         <Link to="/" >
             <img src={logo} alt="logo" width={160} height={32} loading="lazy" />
         </Link>
@@ -16,26 +16,27 @@ const Navbar = (props) => {
         {/* Home-about-contact  */}
 
         <nav >
-            <ul className='flex gap-3'>
+            <ul className='flex gap-x-6 text-richblack-25 '>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/">About</Link>
+                    <Link to="">About</Link>
                 </li>
                 <li>
-                    <Link to="/">Contact</Link>
+                    <Link to="">Contact</Link>
                 </li>
             </ul>
         </nav>
 
         {/* login - signup - logout - dashboard */}
 
-       <div className='flex gap-3 ml-5'>
+       <div className='flex items-center gap-x-4 '>
             {   !isLoggedIn &&
                    <Link to="/login">
-                        <button>
-                            Login
+                        <button className='bg-richblack-800 text-richblack-100 py-[8px] px-[12px]
+                            rounded-[8px] border border-richblack-700 ' >
+                            Log in
                         </button>
                     </Link>
                 }
@@ -43,7 +44,8 @@ const Navbar = (props) => {
                 {
                     !isLoggedIn &&  
                     <Link to="/signup">
-                        <button>
+                        <button className='bg-richblack-800 text-richblack-100 py-[8px] px-[12px]
+                            rounded-[8px] border border-richblack-700 '>
                             Sign up
                         </button>
                     </Link>
@@ -52,7 +54,8 @@ const Navbar = (props) => {
                 {
                     isLoggedIn &&
                     <Link to="/">
-                        <button onClick={() => {
+                        <button className='bg-richblack-800 text-richblack-100 py-[8px] px-[12px]
+                            rounded-[8px] border border-richblack-700 ' onClick={() => {
                             setIsLoggedIn(false);
                             toast.success("Logged Out");
                         }}>
@@ -64,7 +67,8 @@ const Navbar = (props) => {
                 {
                     isLoggedIn &&
                     <Link to="/dashboard">
-                        <button>
+                        <button className='bg-richblack-800 text-richblack-100 py-[8px] px-[12px]
+                            rounded-[8px] border border-richblack-700 '>
                             Dashboard
                         </button>
                     </Link>
