a2c331c8fe85764b37faca9538975add6e7844e3
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-01 10:16:57 +0530

    Added tailwind CSS

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className=' flex justify-center items-center h-full text-white text-3xl' 
+    <div className=' flex justify-center items-center flex-1 text-white text-3xl ' 
     >Welcome to Studynotion!</div>
   )
 }
