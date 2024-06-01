518ca219f3f43424010b0c01de9892bb45c09e1b
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-01 10:17:18 +0530

    Added tailwind CSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className=' flex justify-center items-center text-white text-3xl h-full '
+    <div className=' flex justify-center items-center flex-1 text-white text-3xl ' 
     >Home</div>
   )
 }
